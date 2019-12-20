8e14c3050c9624ae6608a92828493edd8cbda849
marvermar <m.veramartin@gmail.com>
2019-12-20 17:11:02 +0100

    create docs

---
+++ docs/precache-manifest.56b030a5a3e8c3d0f3ecc1daec882ad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba50b405ce8bc0d289e57b91e28332a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dcb43e2ea015be2698ac",
+    "url": "./static/css/main.e42e9ed6.chunk.css"
+  },
+  {
+    "revision": "791f43f1e59c71ba75f8",
+    "url": "./static/js/2.17133985.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.17133985.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcb43e2ea015be2698ac",
+    "url": "./static/js/main.25e5d310.chunk.js"
+  },
+  {
+    "revision": "237a826452db7f8966a3",
+    "url": "./static/js/runtime-main.f77a52cf.js"
+  }
+]);
